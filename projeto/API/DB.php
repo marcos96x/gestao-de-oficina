fe78437b049204a12d17bc8dc709a1e2050760a5
Marcos Alexandre <marcos@phpstaff.com.br>
2021-06-14 19:45:55 -0300

    senha banco

--- projeto/API/DB.php
+++ projeto/API/DB.php
@@ -1,17 +1,12 @@
 <?php
     
     function connect() {
-        // $host = "db_qrcode_test.mysql.dbaas.com.br";
-        // $user = "db_qrcode_test";
-        // $pass = "P@ulO#675514!$";
-        // $db = "db_qrcode_test";
 
 
         $host = "localhost";
         $user = "root";
         $pass = "root";
         $db = "db_oficina";
-        // db_schindler, P@ulO#675514!$
         try {
             $con = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
